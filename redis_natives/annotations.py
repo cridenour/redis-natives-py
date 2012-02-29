d10c09f5475304a94bf987d455b02556316fdba3
Konsta Vesterinen <konsta.vesterinen@gmail.com>
2012-02-29 18:18:26 +0200

    some minor fixes

--- redis_natives/annotations.py
+++ redis_natives/annotations.py
@@ -1,5 +1,5 @@
 """
-Annotations/hooks that alter the creation process of ``Redis natives``. 
+Annotations/hooks that alter the creation process of ``Redis natives``.
 """
 
 __version__ = '0.11'
@@ -20,9 +20,10 @@ __all__ = (
 def _temporaryWrapper(attr, arg, cls):
     def hook(rDatatype):
         getattr(rDatatype, attr)(arg)
-        return rDatatype    
+        return rDatatype
     cls.after_create.append(hook)
     return cls
+
 
 def temporary(after=None, at=None):
     """Marks created keys as volatile. They will be destroyed automatically
@@ -30,7 +31,7 @@ def temporary(after=None, at=None):
     """
     if after is at is None:
         return lambda cls: cls
-    else:        
+    else:
         if (after is None):
             attr = "let_expire_at"
             arg = at
@@ -39,10 +40,12 @@ def temporary(after=None, at=None):
             arg = after
         return partial(_temporaryWrapper, attr, arg)
 
+
 def _namespacedWrapper(ns, sep, cls):
     cls.before_create.append(lambda key: ns + sep + key)
     return cls
-    
+
+
 def namespaced(ns, sep=":"):
     """Embeds all created in a given namespace by prefixing their
     keys with namespace ``ns`` and separator ``sep``.
@@ -51,12 +54,14 @@ def namespaced(ns, sep=":"):
         return partial(_namespacedWrapper, ns, sep)
     return lambda cls: cls
 
+
 def _indexedWrapper(rSet, cls):
     def hook(rDatatype):
         rSet.add(rDatatype.key)
         return rDatatype
     cls.after_create.append(hook)
     return cls
+
 
 def indexed(idxSet):
     """Keeps track of all created keys by adding them to the ``RedisSet``
@@ -67,12 +72,14 @@ def indexed(idxSet):
     else:
         return partial(_indexedWrapper, idxSet)
 
+
 def _incrementalWrapper(rPrim, cls):
     def hook(rDatatype):
         rPrim.incr()
         return rDatatype
     cls.after_create.append(hook)
     return cls
+
 
 def incremental(rPrim):
     """Increments ``RedisPrimitive`` ``rPrim`` by value 1 for each
@@ -81,23 +88,25 @@ def incremental(rPrim):
     if not isinstance(rPrim, Primitive):
         return lambda cls: cls
     else:
-        return partial(_incrementalWrapper, rPrim) 
+        return partial(_incrementalWrapper, rPrim)
+
 
 def _autonamedWrapper(name, cls):
     cls.before_create.append(lambda key: key + str(name))
     return cls
 
+
 def autonamed(obj):
-    """Instead of passing a key-name for every entity to the Datatype constructor, 
-    you pass an arbitrary object that is representable as ``str`` and everytime 
-    an entity creation is triggered, ``obj`` is asked to return a ``str`` representation 
-    that will be used as key name.
+    """
+    Instead of passing a key-name for every entity to the Datatype constructor,
+    you pass an arbitrary object that is representable as ``str`` and everytime
+    an entity creation is triggered, ``obj`` is asked to return a ``str``
+    representation that will be used as key name.
     """
     if hasattr(obj, "__str__"):
         return partial(_autonamedWrapper, obj)
-    return lambda cls: cls 
+    return lambda cls: cls
 
 
 if __name__ == "__main__":
     pass
-    
