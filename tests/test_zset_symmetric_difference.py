29bc8ed3a3ab377d57ef4037e090cf09c125bc1d
Konsta Vesterinen <konsta.vesterinen@gmail.com>
2012-03-30 17:33:40 +0300

    small fix

--- tests/test_zset_symmetric_difference.py
+++ tests/test_zset_symmetric_difference.py
@@ -8,6 +8,14 @@ class TestIntegerZSet(IntegerZSetTestCase):
         symdiff = self.zset.symmetric_difference(set([(2, 2.0), (3, 3.0)]))
         assert symdiff == set([(1, 1.0), (3, 3.0)])
 
+    def test_symmetric_difference_accepts_redis_sets(self):
+        self.zset.add(1, 1)
+        self.zset.add(2, 2)
+        self.other_zset.add(2, 2)
+        self.other_zset.add(3, 3)
+        assert self.zset.symmetric_difference(self.other_zset) == \
+            set([(1, 1.0), (3, 3.0)])
+
     def test_xor_operator(self):
         self.zset.add(1, 1)
         self.zset.add(2, 2)
@@ -26,10 +34,10 @@ class TestIntegerZSet(IntegerZSetTestCase):
         self.zset ^= set([(2, 2.0), (3, 3.0)])
         assert self.zset.data == [(1, 1.0), (3, 3.0)]
 
-    def test_symmetric_difference_accepts_redis_sets(self):
+    def test_symmetric_difference_update_accepts_redis_sets(self):
         self.zset.add(1, 1)
         self.zset.add(2, 2)
         self.other_zset.add(2, 2)
         self.other_zset.add(3, 3)
-        assert self.zset.symmetric_difference(self.other_zset) == \
-            set([(1, 1.0), (3, 3.0)])
+        self.zset.symmetric_difference_update(self.other_zset)
+        assert self.zset.data == [(1, 1.0), (3, 3.0)]
